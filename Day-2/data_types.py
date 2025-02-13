43c2d0b0ee6698b2f82b4c3584cac17ada45d1d1
Neeraj Kumar <neeraj.kumar@ascendion.com>
2025-02-13 09:46:23 +0530

    update Day-2 learning

---
+++ Day-2/data_types.py
@@ -0,0 +1,34 @@
+# Primitive Data types
+# Strings, Integers, FLoats, Boolean
+
+# Subscripting
+name = "Neeraj"
+print(name[0])
+print(name[-1])
+print(name[2])
+print(name[4])
+
+
+# String
+s = "123"+"345" # Concatenation
+print(s)
+
+# Integer = Whole Number
+i = 123+456
+print(i)
+
+#Large Number
+
+l = 123_456_789 #we can write number by providing underscore , interpreter will ignore the underscore during the runtime
+print(l)
+
+
+#Float = floating point number
+print(3.148)
+
+#Booleans
+
+print(True) # this True False will be the first letter in capital
+print(False)
+
+
---
+++ Day-2/operation.py
@@ -0,0 +1,26 @@
+# Mathematical operation
+
+print(123+345)
+print(4-3)
+print(2 * 3)
+print(4 / 2)
+print(4 // 2) # remainder will remove means no decimal places
+
+print( 2**2)
+print( 2**3) # this is the power like square, cube = Exponents
+
+#PEMDAS LR: this part checked left to right
+
+# () - paranthesis
+# ** - Exponents
+# / or *
+# + or -
+
+print(3*3+3/3-3)
+
+print(3/3*3+3-3)
+print(3*3/3+3-3)
+print(2*3/3+3-3)
+print(3*6/2+3-3)
+
+print(3*(3+3)/3-3)
